e0adc09b0a74f15b101d4d4f9be33117c8baea4b
Amine Hichri <amyn.hichri@gmail.com>
2024-12-13 18:47:50 +0100

    Add Card prototype + Css related to it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PlayingCardComponent } from "./components/playing-card/playing-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [PlayingCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
