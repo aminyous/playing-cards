f0a9a590cdd6868a9cbb9e8ddf53adb23969d1aa
Amine Hichri <amyn.hichri@gmail.com>
2024-12-13 19:40:02 +0100

    Use InputSignal

--- src/app/components/playing-card/playing-card.component.ts
+++ src/app/components/playing-card/playing-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, Input, InputSignal } from '@angular/core';
 import { Monster } from '../../models/monster.model';
 
 @Component({
@@ -9,19 +9,14 @@ import { Monster } from '../../models/monster.model';
   styleUrl: './playing-card.component.css',
 })
 export class PlayingCardComponent {
-  @Input(
-    // { required: true }
-    // { alias: "my-monster"}
-    { transform : (value: Monster) => {
-      value.hp = value.hp / 2;
-      return value;
-    }}
-  ) monster: Monster = new Monster();
-  // @Input() name: string = 'My Monster';
-  // @Input() hp: number = 40;
-  // @Input() figureCaption: string = 'N°001 Monster';
-  // @Input() attackName: string = 'Geo Impact';
-  // @Input() attackStrength: number = 60;
-  // @Input() description: string = "This is a long description of a monster capacity. \
-  //               Probably something to do with electricity."
+   //monster: InputSignal<Monster> = input.required();
+  //  monster: InputSignal<Monster> = input(new Monster(), {
+  //     alias: "my-monster",
+  //     transform: (value: Monster) => {
+  //       value.hp = value.hp / 2;
+  //       return value;
+  //     }
+  //  });
+   monster: InputSignal<Monster> = input(new Monster());
+
 }
